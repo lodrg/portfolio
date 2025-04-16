8d88edce8fe9f21c1b035276a88d9705d44e1a57
lodrg <327733082@qq.com>
2025-04-16 22:30:22 +0800

    blogs images gallery

--- src/data/blogs.ts
+++ src/data/blogs.ts
@@ -3,21 +3,21 @@ import { Blog } from '../types/blog';
 
 export const blogs: Blog[] = [
   {
-    id: "portfolio-website",
+    id: "1",
     title: "Blog 1",
     description: "get started with blog 1",
     thumbnail: "/images/projects/portfolio.jpg",
     content: "content of blog 1",
   },
   {
-    id: "go-db-project",
+    id: "2",
     title: "GoDB Database System",
     description: "A custom database system built with Go from scratch, featuring high performance and scalability.",
     thumbnail: "/images/projects/godb.jpg",
     content: "content of blog 2",
   },
   {
-    id: "ml-project",
+    id: "3",
     title: "Machine Learning Model",
     description: "A machine learning model for image classification built with PyTorch.",
     thumbnail: "/images/projects/ml-project.jpg",
