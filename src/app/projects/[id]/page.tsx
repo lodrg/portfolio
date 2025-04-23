087b1d1a9716f2c3aa2137f1faac857ae13c807c
lodrg <327733082@qq.com>
2025-04-23 10:52:38 +0800

    fix build

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -4,8 +4,8 @@ import { notFound } from 'next/navigation';
 import ProjectDetails from './ProjectDetails';
 
 interface ProjectPageProps {
-  params: { id: string; }
-  searchParams?: { [key: string]: string | string[] | undefined };
+  params: Promise<{ id: string; }>
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export function generateStaticParams() {
@@ -14,12 +14,13 @@ export function generateStaticParams() {
   }));
 }
 
-export default function ProjectPage({ params }: ProjectPageProps) {
+export default async function ProjectPage(props: ProjectPageProps) {
+  const params = await props.params;
   const project = projects.find((p) => p.id === params.id);
-  
+
   if (!project) {
     notFound();
   }
-  
+
   return <ProjectDetails project={project} />;
 }
